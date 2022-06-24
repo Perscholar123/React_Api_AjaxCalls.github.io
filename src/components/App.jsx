44c28167f45bb8207de0bba5f69be3897a084168
Perscholar123 <praveena.selvi@gmail.com>
2022-06-24 04:22:17 -0400

    Api Calls

---
+++ src/components/App.jsx
@@ -0,0 +1,50 @@
+
+// 1. IMPORT REACT
+import React from "react";
+// 2. ADDITIONAL IMPORTS
+import Users from "./Users";
+// 3. CREATE COMPONENT
+const App = () => {
+  return (
+    <div className="ui container" style={{ marginTop: "30px" }}>
+      <Users />
+    </div>
+  );
+};
+// 4. EXPORT COMPONENT
+export default App;
+
+// // 1. IMPORT REACT
+// import React from "react";
+// // 2. ADDITIONAL IMPORTS
+// import Users from "./Users";
+// // 3. CREATE COMPONENT
+// const App = () => {
+//   return (
+//     <div>
+//       <Users />
+//     </div>
+//   );
+// };
+// // 4. EXPORT COMPONENT
+// export default App;
+
+
+
+
+
+// import React from 'react';
+
+
+
+// class App extends React.Component {
+//     render(){
+//         return(
+//             <div>
+//                 <h1>Hello</h1>
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
---
+++ src/index.jsx
@@ -0,0 +1,31 @@
+// 1. IMPORT REACT
+import React from "react";
+// 2. IMPORT REACTDOM
+import ReactDOM from "react-dom";
+// 3. ADDITIONAL IMPORTS
+import App from "./components/App";
+// 4. RENDER
+ReactDOM.render(<App />, document.querySelector("#root"));
+
+
+
+
+
+
+
+// // 1. IMPORT REACT
+// import React from "react";
+// // 2. IMPORT REACTDOM
+// import ReactDOM from "react-dom";
+// // 3. ADDITIONAL IMPORTS
+// import App from "./components/App";
+// // 4. RENDER
+// ReactDOM.render(<App />, document.querySelector("#root"));
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/App';
+
+// ReactDOM.render(<App />,document.getElementById('root'));
